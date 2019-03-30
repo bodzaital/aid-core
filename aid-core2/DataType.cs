4abe117b5ce4f915daed10db8deb841024087fd5
Zsolt Boda <boda0zsolt@live.com>
2019-03-30 11:05:16 +0100

    Bunch of work
    Can isolate high res images.

---
+++ aid-core2/DataType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aid_core2
+{
+	class DataType
+	{
+
+	}
+}
--- aid-core2/Program.cs
+++ aid-core2/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.IO.Compression;
 using HtmlAgilityPack;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace aid_core2
 {
@@ -15,25 +16,77 @@ namespace aid_core2
 	{
 		static void Main(string[] args)
 		{
-			string url = "https://www.amazon.com/French-Connection-Whisper-Sleeveless-Strappy/dp/B07BFRVY11";
+			Uri uri = new Uri("https://www.amazon.com/French-Connection-Whisper-Sleeveless-Strappy/dp/B07BFRVY11");
 
-			string source = GetSource(url);
+			string[] uris = GetImageLinks(uri);
+			
+			foreach (string link in uris)
+			{
+				Console.WriteLine(link);
+			}
 
-			Console.WriteLine(source);
+			Console.WriteLine("Done.");
+			Console.ReadLine();
 		}
 
-		private static string GetSource(string url)
+		private static string GetSource(Uri uri)
 		{
-			using (WebClient web = new WebClient())
+			using (WebClient client = new WebClient())
 			{
-				web.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
+				// Download source.
+				Console.WriteLine("Downloading source.");
+				client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
+				byte[] byteStream = client.DownloadData(uri);
+
+				// Inflate with gzip.
+				Console.WriteLine("Inflating.");
+				GZipStream gzip = new GZipStream(new MemoryStream(byteStream), CompressionMode.Decompress);
+				MemoryStream inflatedStream = new MemoryStream();
+				gzip.CopyTo(inflatedStream);
+
+				// Set encoding.
+				Console.WriteLine("Decoding.");
 				Encoding encoding = Encoding.Default;
-				byte[] data = web.DownloadData(url);
-				GZipStream gzip = new GZipStream(new MemoryStream(data), CompressionMode.Decompress);
-				MemoryStream decompressed = new MemoryStream();
-				gzip.CopyTo(decompressed);
-				return encoding.GetString(decompressed.GetBuffer(), 0, (int)decompressed.Length);
+				return encoding.GetString(inflatedStream.GetBuffer(), 0, (int)inflatedStream.Length);
 			}
+		}
+
+		private static string[] GetImageLinks(Uri uri)
+		{
+			string source = GetSource(uri);
+
+			const string START_DETECT = "P.when('A').register(\"ImageBlockATF\", function(A){";
+			const string END_DETECT = "A.trigger('P.AboveTheFold'); // trigger ATF event.";
+			const string REFINED_START_DETECT = "{";
+			const string REFINED_END_DETECT = "};";
+
+			// Initial substring to isolate the data.
+			Console.WriteLine("Isolating (1).");
+			int start = source.IndexOf(START_DETECT) + START_DETECT.Length;
+			int end = source.IndexOf(END_DETECT, start);
+			string substring = source.Substring(start, end - start);
+
+			// Refined substring so only the json object remains.
+			Console.WriteLine("Isolating (2).");
+			start = substring.IndexOf(REFINED_START_DETECT);
+			end = substring.IndexOf(REFINED_END_DETECT, start) + REFINED_END_DETECT.Length;
+			substring = substring.Substring(start, end - start - 1);
+
+			// Deserializing the JSON object to a dynamic variable.
+			Console.WriteLine("Deserializing.");
+			dynamic a = JsonConvert.DeserializeObject(substring);
+			string[] uris = new string[a.colorImages.initial.Count];
+			for (int i = 0; i < uris.Length; i++)
+			{
+				uris[i] = a.colorImages.initial[i].hiRes;
+			}
+
+			return uris;
+		}
+
+		private static void DownloadImages(Uri[] uris)
+		{
+
 		}
 	}
 }
